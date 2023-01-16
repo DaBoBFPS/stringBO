d93c6fb539b4d339d009649e968d5877636ce1ce
DaBoBFPS <121805656+DaBoBFPS@users.noreply.github.com>
2023-01-16 11:49:32 +0300

    Create CALC.go

---
+++ CALC.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	errInvalidType   = errors.New("Данный оператор не поддреживается между этими типами")
+	errStringInvalid = errors.New("Строка имела неверный формат")
+	errMaxValue      = errors.New("Число не должно превышать 10")
+)
+
+func process_string(str string) (string, string, string, bool, error) {
+	var l, r, s string
+	if string(str[0]) == "\"" {
+		l = str[1:]
+		l = l[:strings.Index(l, "\"")]
+		_, cutted, _ := strings.Cut(str, l)
+		s = string(cutted[2])
+		cutted = cutted[1:]
+		if strings.Index(cutted, "\"") == -1 {
+			splitted := strings.Split(cutted, " ")
+			s = splitted[1]
+			r = splitted[2]
+			r = r[:len(r)-1]
+			return l, r, s, true, nil
+		} else {
+			cutted = cutted[1:]
+			r = cutted[strings.Index(cutted, "\"")+1 : len(cutted)-2]
+			return l, r, s, false, nil
+		}
+
+	} else {
+		panic(errStringInvalid)
+	}
+}
+
+func subString(s, n string) string {
+	res, err := strconv.Atoi(n)
+	if err != nil {
+		panic(err)
+	}
+	s = s[:len(s)/res]
+	return s
+}
+func multString(l, n string) string {
+	m, err := strconv.Atoi(n)
+	if err != nil {
+		panic(err)
+	}
+	res := ""
+	for i := 0; i < m; i++ {
+		res += l
+	}
+	if len(res) > 40 {
+		return res[:40] + "..."
+	} else {
+		return res
+	}
+}
+
+func calculate(l, r, m string, isNum bool) (string, error) {
+	switch m {
+	case "+":
+		if isNum {
+			return "", errInvalidType
+		}
+		return l + r, nil
+
+	case "-":
+		if isNum {
+			return "", errInvalidType
+		}
+		before, after, found := strings.Cut(l, r)
+		if found {
+			return after, nil
+		} else {
+			return before, nil
+		}
+
+	case "*":
+		if !isNum {
+			return "", errInvalidType
+		}
+		return multString(l, r), nil
+
+	case "/":
+		if !isNum {
+			return "", errInvalidType
+		}
+		return subString(l, r), nil
+
+	default:
+		return "", errors.New("Такой операции нет")
+	}
+}
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = text[:len(text)-1]
+	left, right, middle, isNum, err := process_string(text)
+	if err != nil {
+		panic(err)
+	}
+	val, _ := strconv.Atoi(right)
+	if isNum && val > 10 {
+		panic(errMaxValue)
+	}
+	res, err := calculate(left, right, middle, isNum)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
+}
